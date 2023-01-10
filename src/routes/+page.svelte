9a66a7c62238bf18a828f2747f4c06c01855d5fb
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2023-01-10 17:30:11 +0100

    looking for sources and assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="https://www.gov.ie/en/publication/57d2a-forestry-facts-and-news/#forestry-facts-and-news">Facts about forests</a>
