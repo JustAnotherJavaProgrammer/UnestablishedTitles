a7fd55f4c111c018a7b492c3ceb5bf06623a1afd
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2023-01-15 04:10:39 +0100

    Working on the header

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,6 +1,83 @@
 <script lang="ts">
-    import Image from "../../public/img/Greek_Roman_Laurel_wreath_with_branches_vector.svg"
-    console.log(Image)
+    import wreathIcon from "$lib/assets/img/Greek_Roman_Laurel_wreath_with_branches_vector.svg";
+    export let black: boolean = false;
 </script>
 
-<nav></nav>
+<nav class:black>
+    <div id="container">
+        <a id="rootlink" href="/">
+            <img id="icon" src={wreathIcon} alt="A laurel wreath with branches" />
+            <span id="title">Unestablished Titles</span>
+        </a>
+        <div id="subpages">
+            <a href="/">Home</a>
+            <a href="/">Get now</a>
+            <a href="/">Everyday sale</a>
+        </div>
+    </div>
+</nav>
+
+<style>
+    .black a,
+    .black {
+        color: black;
+    }
+
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding-top: 1rem;
+        font-family: "Bebas Neue", sans-serif;
+        letter-spacing: 0.075rem;
+        text-transform: uppercase;
+        width: 100%;
+        max-width: 1050px;
+    }
+
+    #container {
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    #subpages {
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        align-items: center;
+        margin-left: 1rem;
+        font-size: 1.175rem;
+        letter-spacing: 0.075em;
+        gap: 2rem;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    #rootlink {
+        font-family: "Old Standard TT", serif;
+        letter-spacing: normal;
+        font-size: 1.375rem;
+        vertical-align: bottom;
+    }
+
+    .black #icon {
+        filter: brightness(0);
+    }
+
+    #icon {
+        height: 2rem;
+        width: 2rem;
+        object-fit: contain;
+        object-position: center;
+        vertical-align: bottom;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,19 @@
+<script>
+    import { building } from "$app/environment";
+    import Header from "$lib/Header.svelte";
+    import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<Header>
+    <svelte:fragment slot="navbar"><Navbar /></svelte:fragment>
+    <svelte:fragment slot="hero">You may wish to save the Irish woodlands</svelte:fragment>
+    <svelte:fragment slot="title">Call Yourself a Lord Today</svelte:fragment>
+    <svelte:fragment slot="subtitle"
+        >(An absolutely useless gift
+        {#if !building} in {new Date().getFullYear()}{/if}
+        )</svelte:fragment
+    >
+</Header>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="https://www.gov.ie/en/publication/57d2a-forestry-facts-and-news/#forestry-facts-and-news">Facts about forests</a>
