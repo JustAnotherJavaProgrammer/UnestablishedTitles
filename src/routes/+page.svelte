193f45b80790fca4e59c786c6d2b64e91a1a20ae
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2023-03-04 01:57:09 +0100

    Renamed an image to help the Actions Runner find it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,9 @@
     import TestimonialPassport from "$lib/assets/img/testimonials/nicole-geri-gMJ3tFOLvnA-unsplash.jpg";
     // @ts-ignore
     import TestimonialPassport_srcset from "$lib/assets/img/testimonials/nicole-geri-gMJ3tFOLvnA-unsplash.jpg?w=500;900;1200;2000&webp&srcset";
-    import TestimonialTinder from "$lib/assets/img/testimonials/IMG_0836.jpg";
+    import TestimonialTinder from "$lib/assets/img/testimonials/tinder.jpg";
     // @ts-ignore
-    import TestimonialTinder_srcset from "$lib/assets/img/testimonials/IMG_0836.jpg?w=500;900;1200;2000&webp&srcset";
+    import TestimonialTinder_srcset from "$lib/assets/img/testimonials/tinder.jpg?w=500;900;1200;2000&webp&srcset";
     import MetaInfo from "$lib/elements/MetaInfo.svelte";
 </script>
 
