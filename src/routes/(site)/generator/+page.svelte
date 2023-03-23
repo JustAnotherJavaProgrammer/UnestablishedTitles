c49b98f47721946583796b3ff59fadecb94b7f36
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2023-03-24 00:05:19 +0100

    Fixed the thank you message after certificate generation

--- src/routes/(site)/generator/+page.svelte
+++ src/routes/(site)/generator/+page.svelte
@@ -115,6 +115,20 @@
         <div class="message">
             <ParagraphSerif>
                 <svelte:fragment slot="title">Do you like this site?</svelte:fragment>
+                <p>
+                    Unestablished Titles is a free service, and it probably always will be. But I used the free software and media created by others before me. This project
+                    wouldn't have been possible without them. So, you might want to <a href="/legal/credits">check out the credits</a>, and thank them for their effort.
+                </p>
+                <p>
+                    You might also wish to actually contribute to protecting the Irish woodlands. According to a quick Google search and their Wikipedia article, you can do so
+                    by donating to the <a href="https://www.nativewoodlandtrust.ie/" target="_blank">Native Woodland Trust</a>.
+                </p>
+                <p>
+                    If you want to support me financially, you can do so by making a one-time donation via Ko-fi. But that's really not necessary. As a student, I don't have any
+                    running costs for operating this website, and I would just use the money for myself. You'll get nothing in return. Not even that fuzzy feeling in your
+                    stomach, when you know that you've done something good, like helping a person in need or donating to charity. Still, if you wish to spend your money that
+                    way, I'm not here to stop you. <a href="https://ko-fi.com/justanotherjavaprogrammer" target="_blank">Tip via Ko-fi.</a>
+                </p>
             </ParagraphSerif>
         </div>
     {/if}
